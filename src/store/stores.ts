05e0b01f3abfdfa6134d8ad4e5c544305681a9a1
Sonukumar9117 <krsonu1791@gmail.com>
2025-08-02 15:23:01 +0530

    Feat:data-persist

--- src/store/stores.ts
+++ src/store/stores.ts
@@ -1,9 +1,16 @@
 import {combineReducers, createStore} from 'redux';
 import userReducer from './userReducer';
+import { persistReducer,persistStore } from 'redux-persist';
+import storage from '@react-native-async-storage/async-storage'
+const persistConfig={
+    key:"root",
+    storage
+}
 const rootReducer=combineReducers({ 
     // this is helpful when there is multiple reduce in our app then here we combile all the reducer. Reducer is a function whose work is to update state and store it 
-    userData:userReducer
+    userData:persistReducer(persistConfig,userReducer)
 });
 export const store=createStore(rootReducer);
 
 export type RootState=ReturnType<typeof rootReducer>
+export const persistor=persistStore(store)
